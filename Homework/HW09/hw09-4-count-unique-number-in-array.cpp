df047e81f70d6a60dbcdc227ad1fa7708c22af95
Muad2003 <anuphap2003118@gmail.com>
2023-09-14 03:26:59 +0700

    update homework 9-4

--- Homework/HW09/hw09-4-count-unique-number-in-array.cpp
+++ Homework/HW09/hw09-4-count-unique-number-in-array.cpp
@@ -47,3 +47,50 @@
         7 -> 1 value.
         9 -> 2 values.
 */
+
+#include <stdio.h>
+
+int main() {
+    
+    int n ;
+    int temp = 0 ;
+    printf ( "Input N :\n" ) ;
+    scanf ( "%d", &n ) ;
+    int array[ n ] ;
+    int count[ n ] = {  } ;
+
+    // รับค่า ตามจำนวน n
+    for ( int i = 0 ; i < n ; i++ ) {
+        printf ( "Element[%d] :\n", i ) ;
+        scanf ( "%d", &array[ i ]  ) ;
+    } // end for
+
+    // เรียงค่าจากเลขน้อยไปหามาก
+    for ( int i = 0 ; i < n ; i++ ) {
+        for ( int k = i + 1 ; k < n ; k++ ) {
+            if ( array[ i ] > array[ k ] ) {
+                temp = array[ i ] ;
+                array[ i ] = array[ k ] ;
+                array[ k ] = temp ;
+            } // end if
+        } // end for
+    } // end for
+
+    // ถ้าเปรียบเทียบแล้วได้ค่าซ้ำจะให้ ยอดรวมเพิ่ม
+    for ( int i = 0 ; i <= n ; i++ ) {
+        for ( int k = i + 1 ; k < n ; k++ ) {
+            if ( array[ i ] == array[ k ] ) {
+                count[ i ]++ ;
+            } // end if
+        } // end for
+    } // end for
+    
+    // แสดงค่า
+    for ( int i = 0 ; i < n ; i++ ) {
+        if ( array[ i ] != array [ i - 1 ] ) {
+            printf ( "%d -> %d values.\n", array[ i ] , count[ i ] + 1 ) ;
+        } // end if   
+    } // end for
+    
+    return 0;
+} // end main
